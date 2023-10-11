a3ebabfdafa6d4c4655455ce1ae5bdd165b4e04f
sliupp <simonliu123456789@gmail.com>
2023-10-10 20:10:16 -0700

    Added Starter Header Component

---
+++ portfolio_website/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Building Castles in the Cloud with Lines of Code.</h1>
+            <button>Let's Dive In</button>
+        </div>
+    )
+}
